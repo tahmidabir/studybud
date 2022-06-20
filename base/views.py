6add6d22c533edfe84ff367fd8b82efb56924621
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-06-21 03:20:03 +0600

    second commit

--- base/views.py
+++ base/views.py
@@ -1,9 +1,30 @@
 from django.shortcuts import render
 
+rooms=[
+    {'id': 1, 'name': 'Lets learn python'},
+    {'id': 2, 'name': 'Lets learn django'},
+    {'id': 3, 'name': 'Frontend developer'},
+
+
+]
+
+users=[
+    {'name':'Abir','id':1},
+    {'name':'Sama','id':2},
+    {'name':'Imtiaz','id':3}
+
+]
+
 
 def home(request):
-    return render(request,'home.html')
+    params={'rooms':rooms}
+    return render(request,'base/home.html', params)
 
 
-def room(request):
-    return render(request, 'room.html')
+def room(request,pk):
+    room=None
+    for i in rooms:
+        if i['id']==int(pk):
+            room=i
+        context={'room':room}
+    return render(request, 'base/room.html',context)
