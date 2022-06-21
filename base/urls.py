76c83e04bd23b4954204fcc2f6f07fb8d2a5b34a
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-06-21 22:59:50 +0600

    added some more models and made relations among them

--- base/urls.py
+++ base/urls.py
@@ -1,11 +1,13 @@
-
 from django.contrib import admin
 from django.urls import path
 
 from . import views
 
 urlpatterns = [
-    path('', views.home,name='home'),
-    path('room/<str:pk>/', views.room,name='room'),
+    path('', views.home, name='home'),
+
+    # see information about a specific room passing a dynamic value
+
+    path('room/<str:pk>/', views.room, name='room'),
 
 ]
--- base/views.py
+++ base/views.py
@@ -1,30 +1,16 @@
 from django.shortcuts import render
+from .models import Room
 
-rooms=[
-    {'id': 1, 'name': 'Lets learn python'},
-    {'id': 2, 'name': 'Lets learn django'},
-    {'id': 3, 'name': 'Frontend developer'},
-
-
-]
-
-users=[
-    {'name':'Abir','id':1},
-    {'name':'Sama','id':2},
-    {'name':'Imtiaz','id':3}
-
-]
 
 
 def home(request):
-    params={'rooms':rooms}
-    return render(request,'base/home.html', params)
+
+    rooms=Room.objects.all()
+    context={'rooms':rooms}
+    return render(request,'base/home.html',context)
 
 
 def room(request,pk):
-    room=None
-    for i in rooms:
-        if i['id']==int(pk):
-            room=i
-        context={'room':room}
+    room=Room.objects.get(id=pk)
+    context={'room':room}
     return render(request, 'base/room.html',context)
