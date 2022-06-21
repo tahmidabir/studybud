7d6cb3b45bb6050ca4e5ee5c688acc73385e066b
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-06-22 01:54:54 +0600

    side bar working perfectly

--- base/views.py
+++ base/views.py
@@ -3,8 +3,10 @@ from .models import Room,Topic
 from .forms import RoomForm
 
 def home(request):
+    q = request.GET.get('q') if request.GET.get('q')!=None else ''
 
-    rooms=Room.objects.all()
+
+    rooms=Room.objects.filter(topic__name__icontains=q)
 
     topics = Topic.objects.all()
 
