c1bb5b3e4e7ce10f1c5bb11296b73409ddbf0e72
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-06-23 01:49:01 +0600

    added user profile and linked their name in home page with their profile

--- base/urls.py
+++ base/urls.py
@@ -12,6 +12,7 @@ urlpatterns = [
     # see information about a specific room passing a dynamic value
 
     path('room/<str:pk>/', views.room, name='room'),
+    path('profile/<str:pk>/', views.userProfile, name='user-profile'),
     path('create-room/', views.createRoom, name='create-room'),
     path('update-room/<str:pk>', views.updateRoom, name='update-room'),
     path('delete-room/<str:pk>', views.deleteRoom, name='delete-room'),
