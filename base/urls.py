ee84a30c11eef5683e98eaf4f6460ef430f7050e
tahmidabir <md.tahmid.chowdhury.abir@g.bracu.ac.bd>
2022-06-27 02:43:52 +0600

    configured recent activity for mobile view

--- base/urls.py
+++ base/urls.py
@@ -19,5 +19,6 @@ urlpatterns = [
     path('delete-message/<str:pk>', views.deleteMessage, name='delete-message'),
     path('update-user/', views.updateUser, name='update-user'),
     path('topics/', views.topicsPage, name='topics'),
+    path('activity/', views.activityPage, name='activity'),
 
 ]
